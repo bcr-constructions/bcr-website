e614344942acca4d3dd22dd2e271f27215e14b6a
abdullahchattha988-hue <abdullahchattha988@gmail.com>
2026-03-12 11:46:57 +0500

    xc

--- client/components/ScrollToTop.tsx
+++ client/components/ScrollToTop.tsx
@@ -5,11 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const root = document.getElementById("root");
-    if (root) root.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    document.body.scrollTop = 0;
-    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
