3f9b38e08cdf47a4c3c8f88563ac6df67237a284
abdullahchattha988-hue <abdullahchattha988@gmail.com>
2026-03-13 17:22:40 +0500

    wwwww

--- client/main.tsx
+++ client/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = document.getElementById("root");
@@ -8,8 +7,4 @@ if (!root) {
   throw new Error("Root element not found");
 }
 
-createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+createRoot(root).render(<App />);
